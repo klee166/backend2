6a699bfd2479c445d0e476c35804df807d432004
Kwang Bin Lee <klee166@jhu.edu>
2016-10-31 05:44:51 -0400

    commit

--- app/models/filter.rb
+++ app/models/filter.rb
@@ -1,3 +1,12 @@
 class Filter < ApplicationRecord
   belongs_to :user
+  attr_accessor :isOn, :preferences
+
+  def isOn=(val)
+    @isOn = val
+  end
+
+  def setEventType(val)
+    @preferences.push(val)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,27 @@
 class User < ApplicationRecord
-  has_one :filter
 
-  
+  has_one :filter
+  attr_accessor :isConfirmed, :confirmationCode
+
+
+  def confirmationCode=(val)
+    @confirmationCode = val
+  end
+
+  def setEventType(val)
+    self.filter.setEventType(val)
+  end
+
+  def isConfirmed=(val)
+    @isConfirmed = val
+  end
+
+  def turn_on_filter
+    self.filter.isOn=true
+  end
+
+  def turn_off_filter
+    self.filter.isOn=false
+  end
+
 end
