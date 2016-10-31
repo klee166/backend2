b388d40ecc3b7ec3a476b23486f12d1e6c815d1e
Kwang Bin Lee <klee166@jhu.edu>
2016-10-31 05:48:55 -0400

    commit

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -1,3 +1,14 @@
 class Offer < ApplicationRecord
   belongs_to :event
+  attr_reader :name, :numUsed, :maxUse
+  attr_accessor :numUsed
+
+  def initialize(name, numUsed, maxUse)
+    @name, @numUsed, @maxUse = name, numUsed, maxUse
+  end
+
+  #increment the number of used ones
+  def increase_used
+    self[:numUsed] += 1
+  end
 end
